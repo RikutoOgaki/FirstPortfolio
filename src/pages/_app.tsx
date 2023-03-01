308586ec8941e232475aa9ecab115ca17faafdd5
Rikuto <orikutobasuke@gmail.com>
2023-03-01 11:32:50 +0900

    環境構築中

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/provider'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return 
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
 }
+export default App
---
+++ src/pages/top.tsx
@@ -0,0 +1,10 @@
+import {Box} from '@chakra-ui/react'
+
+
+export default function num2(){
+    return(
+        <div>
+            <Box as='main' w={100} h={100} bg={'#fff'}></Box>
+        </div>
+    )
+}
