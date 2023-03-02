a0fe894671771b81591e6b0537de76cb74bafd48
Rikuto <orikutobasuke@gmail.com>
2023-03-02 14:53:13 +0900

    portfolio add

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import {Box,Text,Flex} from '@chakra-ui/react'
+import NextLink from 'next/link'
+
+export default function Footeer(){
+    return(
+        <></>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,22 @@ import Header from '@/components/Header'
 export default function Home(){
     return(
         <Box bg={"#1a4472"}>
-          <Box as='header'>
             <Header/>
             <Flex justifyContent={"center"} alignItems={"center"} w={"100%"} h={"600px"}>
               <Text color={"#fff"} fontWeight={"bold"} fontSize={"90px"}>Rikuto Ogaki</Text>
             </Flex>
-          </Box>
-          <Box as='main'>
+          <Box as='main' marginTop={"180px"}>
             <Box as='div'>
-              <Text as={"h1"} color={"#fff"} fontSize={"4.8em"}>大垣 陸斗</Text>
+              <Box as='span' color={"#fff"}>おおがき りくと</Box>
+              <Text as={"h1"} color={"#fff"} fontSize={"6rem"}>大垣 陸斗</Text>
                 <Text as={"p"} color={"#fff"}>
                   2003年5月31日生まれ<br/>
                   ECCコンピュータ専門学校 Webデザインコース<br/>
                   25年卒
                 </Text>
-                <Text as={"p"}>
-                  
+                <Text as={"p"} color={"#fff"}>
+                  ようこそ。私のポートフォリオへ。<br/>
+                  私の名前は大垣陸斗といいます。現在、ECCコンピュータ専門学校のWebデザインコースでWebのことについて勉強しています。<br/>
                 </Text>
             </Box>
           </Box>
