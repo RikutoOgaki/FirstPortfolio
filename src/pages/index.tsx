1a0c6beaf3e6229786240ed760876c24723d1181
Rikuto <orikutobasuke@gmail.com>
2023-03-03 16:48:02 +0900

    portfolio add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ export default function Home(){
     return(
         <Box bg={"#1a4472"}>
           <Header/>
+          <Flex justifyContent={"center"} alignItems={"center"} w={"100%"} h={"70rem"}>
+            <Text color={"#fff"} fontWeight={"bold"} fontSize={"90px"}>Rikuto Ogaki</Text>
+          </Flex>
           <Box as='main' marginTop={"180px"}>
-            <Flex justifyContent={"center"} alignItems={"center"} w={"100%"} h={"70rem"}>
-              <Text color={"#fff"} fontWeight={"bold"} fontSize={"90px"}>Rikuto Ogaki</Text>
-            </Flex>
             <Box as='div' marginLeft={"90px"}>
               <Box as='section' >
                 <Box as='span' color={"#fff"} fontSize={"1.6rem"}>おおがき りくと</Box>
@@ -23,7 +23,10 @@ export default function Home(){
               <Box as='section'>
                 <Text as={"p"} color={"#fff"} fontSize={"1.8rem"}>
                   ようこそ。私のポートフォリオへ。<br/>
-                  私の名前は大垣陸斗といいます。<br/>現在、ECCコンピュータ専門学校のWebデザインコースでWebのことについて勉強しています。<br/>
+                  私の名前は大垣陸斗といいます。<br/>
+                  現在、ECCコンピュータ専門学校のWebデザインコースでWebのことについて勉強しています。<br/>
+                  フロントエンドをメインにやっていますが、バックエンドにも興味がありやりたいと考えています。<br/>
+                  ゆくゆくはデザインエンジニアとして活躍していきたいと考えています。
                 </Text>
               </Box>
             </Box>
