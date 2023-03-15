288700e3f333d0cf7e6bb1bda0297f1503dcf974
Rikuto <orikutobasuke@gmail.com>
2023-03-15 12:23:20 +0900

    Firstportfolio add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,10 @@
-import { Box, Text, Flex, border, Container, HStack } from '@chakra-ui/react'
+import { Box, Flex, Text, Center } from '@chakra-ui/react'
 import Link from 'next/link'
-import { NextComponentType } from 'next';
 
 export default function Header() {
     return (
         <>
-            <header>
-                <Flex w={'100vw'} justifyContent={'center'}>
-                    <Container maxW={'container.xl'} position={'absolute'} top={'0'} zIndex={'1'}>
-                        <nav>
-                            <Flex justifyContent={'flex-end'}>
-                                <HStack color={'#fff'} >
-                                    <Link href={'/'}>top</Link>
-                                    <Link href={'/'}>top</Link>
-                                    <Link href={'/'}>top</Link>
-                                </HStack>
-                            </Flex>
-                        </nav>
-                    </Container>
-                </Flex>
-            </header>
+
         </>
     )
 }
