f4babb8f15fb1c994e1c634d00932232488ede85
Rikuto <orikutobasuke@gmail.com>
2023-03-22 00:10:53 +0900

    firstportfolio add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Center, Flex, Text } from '@chakra-ui/react'
+import Head from 'next/head'
+import { Box, Center, Flex, Text, } from '@chakra-ui/react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Name from '@/components/NameBlock'
@@ -9,14 +10,21 @@ import Work from '@/components/WorksBlock'
 export default function portfolio() {
   return (
     <>
-      <Header />
-      <main>
-        <Name />
-        <Profile />
-        <Skill />
-        <Work />
-      </main>
-      <Footer />
+      <Box>
+        <Head>
+          <title>Rikuto Ogaki portfolio</title>
+          <meta name="description" content="Rikuto Ogaki portfolio" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <main>
+          <Name />
+          <Profile />
+          <Skill />
+          <Work />
+        </main>
+        <Footer />
+      </Box>
     </>
   )
 }
